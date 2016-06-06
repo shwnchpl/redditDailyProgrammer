8c8c747473e1c8fcf8a23dc5f13f369abf7d56b5
Shawn M. Chapla <shawnchapla6@gmail.com>
2016-06-06 18:03:27 -0400

    Create Challenge270EASY.java

---
+++ Challenge270/Easy/Challenge270EASY.java
@@ -0,0 +1,27 @@
+// My solution to the problem posted here
+// https://www.reddit.com/r/dailyprogrammer/comments/4msu2x/challenge_270_easy_transpose_the_input_text/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class Challenge270EASY {
+	public static void main(String[] args) {
+		int maxLen = 0;
+		
+		String[] inputArray = (new BufferedReader(
+									new InputStreamReader(System.in)))
+										.lines()
+										.collect(Collectors.toList())
+										.toArray(new String[0]);
+		
+		for(String s : inputArray) if(s.length() > maxLen) maxLen = s.length();
+
+		for(int i = 0; i < maxLen; i++) {
+			for(int j = 0; j < inputArray.length; j++)
+				try { System.out.print(inputArray[j].charAt(i)); } 
+				catch (StringIndexOutOfBoundsException e) { System.out.print(' '); }
+			System.out.print('\n');
+		}
+	}
+}
