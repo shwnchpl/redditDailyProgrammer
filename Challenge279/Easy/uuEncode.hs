091d42a983f928038904999934977dec43a2b318
Shawn M. Chapla <shawnchapla6@gmail.com>
2016-08-18 10:21:00 -0400

    Syntactical changes.
    Made some syntactical changes to improve readability.  Runs exactly the same as initial version.

--- Challenge279/Easy/uuEncode.hs
+++ Challenge279/Easy/uuEncode.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Word(Word8)
+import Data.Monoid((<>))
 import Data.Bits((.&.), shiftL, shiftR)
 import System.Environment(getArgs,getProgName)
 import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Lazy.Char8 as STR (pack,cons,append,unlines)
+import qualified Data.ByteString.Lazy.Char8 as STR (pack,cons,unlines)
 
 transform3 :: [Word8] -> [Word8]
 transform3 [first]                = transform3 [first,0,0]
@@ -21,18 +24,17 @@ partitionBSEach n xs
                       in this : partitionBSEach n rest
 
 shiftBytes :: L.ByteString -> L.ByteString
-shiftBytes = (L.concat . (map (L.pack . transform3)) . (map (L.unpack)) . (partitionBSEach 3))
+shiftBytes = L.concat . map (L.pack . transform3 . L.unpack) . partitionBSEach 3
+
 
 formatString :: L.ByteString -> L.ByteString
-formatString xs = STR.unlines $ map (\x -> let lnChar = (fromIntegral .
-                                                            ((+32) . ceiling . (* (3/4)) .
-                                                                fromIntegral . L.length)) x
-                                           in L.cons lnChar x) (partitionBSEach 60 xs)
+formatString xs = STR.unlines [ lnChar x `L.cons` x | x <- partitionBSEach 60 xs ]
+    where lnChar = fromIntegral . (+32) . ceiling . (*(3/4)) . fromIntegral . L.length
 
 uuEncode :: String ->  L.ByteString -> L.ByteString
-uuEncode fileName xs = header `L.append` (formatString $ shiftBytes xs) `L.append` footer
-    where header = STR.pack $ "begin 644 " ++ fileName ++ "\n"
-          footer = STR.pack "`\nend\n"
+uuEncode fileName xs = header <> (formatString . shiftBytes) xs <> footer
+    where header = "begin 644 " <> STR.pack fileName <> "\n"
+          footer = "`\nend\n" :: L.ByteString
 
 main = do
         args <- getArgs
@@ -44,7 +46,7 @@ main = do
             [input]         -> do
                                 inFile <- L.readFile input
                                 L.putStr $ uuEncode input inFile
-            []              -> L.interact (uuEncode "undefined")
+            []              -> L.interact $ uuEncode "undefined"
             _               -> do
                                 pn <- getProgName
                                 putStrLn $ "Usage: " ++ pn ++ " [infile] [outfile]"
